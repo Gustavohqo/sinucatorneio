bdd1a3902bea6162e0f490bd78a47299e41cc490
Gustavo Oliveira <gustavo.hqo@gmail.com>
2019-10-29 07:51:48 -0300

    Making duos being saved at the firestore

--- functions/src/duo/duo.router.ts
+++ functions/src/duo/duo.router.ts
@@ -1,16 +1,17 @@
 import { Router } from "express";
 import { PlayerService } from "../player/player.service";
 import { Level } from "../player/model/Level";
-import { Player } from "../player/model/Player";
+// import { Player } from "../player/model/Player";
+import { Database } from "../infra/Database";
 
 const duoRouter = Router();
 let duos : any[] = [];
 
 const drawDuos = (players:any) => {
     duos = [];
-    let baianinhoPlayers = randomizeGroup(players.filter((p:Player) =>   p.level == Level.BAIANINHO));
-    let quebraGalhoPlayers = randomizeGroup(players.filter((p:Player) =>   p.level == Level.QUEBRA_GALHO));
-    let samuelPlayers = randomizeGroup(players.filter((p:Player) =>   p.level == Level.SAMUEL));
+    let baianinhoPlayers = randomizeGroup(players.filter((p:any) =>   p.level == Level.BAIANINHO));
+    let quebraGalhoPlayers = randomizeGroup(players.filter((p:any) =>   p.level == Level.QUEBRA_GALHO));
+    let samuelPlayers = randomizeGroup(players.filter((p:any) =>   p.level == Level.SAMUEL));
 
     let spare : any[] = [];
     if (baianinhoPlayers.length !== 0 && baianinhoPlayers.length === samuelPlayers.length) {
@@ -40,26 +41,60 @@ const drawDuos = (players:any) => {
     return duos;
 }
 
-duoRouter.get('/', async (req, res) => {
-    let players = await PlayerService.getPlayers();
-    
-    if(req.query.redo || duos.length === 0 ) {
-        res.send(drawDuos(players));
-    } else {
-        res.send(duos);
-    }
-});
-
-export { duoRouter };
-
 function draw(listOne: any, listTwo: any, duos: any[]) {
     while (listTwo.length > 0 ) {
         const playerOne = listOne.pop();
         const playerTwo = listTwo.pop();
-        duos.push({ playerOne, playerTwo, phase: "", group: "" });
+        if(Boolean(playerOne) && Boolean(playerTwo)) {
+            duos.push({ playerOne, playerTwo, phase: "", group: "" });
+        }
+        else if(! Boolean(playerOne)) {
+            duos.push({ playerTwo, phase: "", group: "" });
+        }
+        else if(! Boolean(playerTwo)){
+            duos.push({ playerOne, phase: "", group: "" });
+        }
     }
 }
 function randomizeGroup(group: any): any {
     return group.slice(0).sort(() => Math.random() - 0.5);
 }
 
+duoRouter.get('/', async (req, res) => {
+    let players = await PlayerService.getPlayers();
+    
+    const collection = Database.getCollection("duos");
+    
+    let duos = await collection.get()
+        .then((snapshot:any) => snapshot.docs.map((doc:any) => {
+            if(doc) {
+                let obj:any = {};
+
+                obj["id"] = doc.id;
+                obj["group"] = doc.data().group;
+                return obj;
+            }
+            return;
+        }));
+
+    if(req.query.redo || duos.length === 0 || duos[0].group.length === 0 ) {
+        let newDuoGroup = drawDuos(players)
+        try {
+            if(duos[0].id) {
+                await collection.doc(duos[0].id).update({group: newDuoGroup})
+            }
+            else {
+                await collection.add({group: newDuoGroup})
+            }
+
+            res.send(newDuoGroup);
+        } catch (error) {
+            res.status(400).send({msg: "erro ao salvar ou realizar update dos pares"});
+        }
+
+    } else {
+        res.send(duos[0].group);
+    }
+});
+
+export { duoRouter };
--- functions/src/player/player.service.ts
+++ functions/src/player/player.service.ts
@@ -14,8 +14,15 @@ export class PlayerService {
         .then((doc:any) => doc.data() as Player);
     }
     
-    static getPlayers() : Player {
+    static getPlayers() : any {
         return this.collection.get()
-        .then((snapshot:any) => snapshot.docs.map((doc:any) => new Player(doc.id, doc.data())));
+        .then((snapshot:any) => snapshot.docs.map((doc:any) => {
+            let player:any = {};
+            player['id'] = doc.id;
+            player['name'] = doc.data().name;
+            player['level'] = doc.data().level;
+            player['nickname'] = doc.data().nickname;
+            return player;
+        }));
     }
 }
