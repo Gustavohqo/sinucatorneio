d2e25a4353fd7b9b14d304a0da015a4b789db2b5
erick.borges@siteware.com.br <erick.borges@siteware.com.br>
2019-10-29 13:27:30 -0300

    estilizando tela de disputas

--- public/campeonato.js
+++ public/campeonato.js
@@ -9,7 +9,7 @@ function getBattleOrder(order,group) {
 }
 
 
-function organizaTorneio(showBattle) {
+function organizaTorneio() {
     fetch('./api.json')
         .then(response => {
             return response.json()
@@ -17,10 +17,8 @@ function organizaTorneio(showBattle) {
         .then(data => {
             let players = data;
             return players.map(function(players, index) {
-                if (showBattle) {
-                    document.getElementById(getBattleOrder(1,index+1)).innerHTML = players.playerOne.name;
-                    document.getElementById(getBattleOrder(2,index+1)).innerHTML = players.playerTwo.name;
-                }
+                document.getElementById(getBattleOrder(1,index+1)).innerHTML = players.playerOne.name;
+                document.getElementById(getBattleOrder(2,index+1)).innerHTML = players.playerTwo.name;
                 document.getElementById(getPlayerOrder(1,index+1)).innerHTML = players.playerOne.name;
                 document.getElementById(getPlayerOrder(2,index+1)).innerHTML = players.playerTwo.name;
             })
@@ -29,4 +27,27 @@ function organizaTorneio(showBattle) {
             console.log(err)
         })
 }
-organizaTorneio(true)
+//organizaTorneio()
+
+let battle = document.getElementsByClassName('battle');
+
+function exibeConfrontos() {
+    document.getElementById('brackets').style.display = 'none';
+    document.getElementById('battles').style.display = 'block'; 
+    let time = 0;
+    for (let i = 0; i < battle.length; i++) {
+        setTimeout(() => {
+            battle[i].classList.add('active');
+            battle[i - 1].classList.remove('active');
+        }, 2000 + time);
+        time += 7000;
+    }
+    setTimeout(() => {
+        last = document.getElementsByClassName('battle active')
+        last[0].classList.remove('active');
+        document.getElementById('brackets').style.display = 'flex';
+        document.getElementById('battles').style.display = 'none';
+    }, 58000);
+}
+
+//exibeConfrontos()
